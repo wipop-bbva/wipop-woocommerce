2bade4ed7f0ea6d891cdda13c1c29ef3162fc2a4
Víctor de Vierna <victor.vierna@gmail.com>
2025-07-04 13:05:57 +0200

    feat(loader): implement Loader class for payment gateways

---
+++ wipop/core/loader.php
@@ -0,0 +1,26 @@
+<?php
+namespace Wipop\Core;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Loader class responsible for registering payment gateways.
+ */
+class Loader {
+
+    public static function init() {
+        add_filter( 'woocommerce_payment_gateways', array( __CLASS__, 'register_gateways' ) );
+    }
+
+    public static function register_gateways( $gateways ) {
+        require_once WIPOP_PLUGIN_PATH . 'gateways/bizum/bizum.php';
+        require_once WIPOP_PLUGIN_PATH . 'gateways/card/card.php';
+        require_once WIPOP_PLUGIN_PATH . 'gateways/googlepay/gpay.php';
+
+        $gateways[] = 'Wipop\Gateways\Bizum\Gateway';
+        $gateways[] = 'Wipop\Gateways\Card\Gateway';
+        $gateways[] = 'Wipop\Gateways\Googlepay\Gateway';
+
+        return $gateways;
+    }
+}
