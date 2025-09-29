3db7611125e9ba4114d4c8c1c8b610fc99821045
Víctor de Vierna <victor.vierna@gmail.com>
2025-09-29 21:00:19 +0200

    chore(QA): port qa configs and helpers from wipop php library (#31)

--- wipop/.php-cs-fixer.dist.php
+++ wipop/.php-cs-fixer.dist.php
@@ -1,24 +1,59 @@
 <?php
 
+declare(strict_types=1);
+
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $finder = Finder::create()
-  ->in(__DIR__)
-  ->exclude('vendor');
+    ->in([
+        __DIR__ . '/admin',
+        __DIR__ . '/core',
+        __DIR__ . '/gateways',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->append([
+        __DIR__ . '/wipop.php',
+    ])
+    ->exclude([
+        'assets',
+        'build',
+        'node_modules',
+        'vendor',
+        'var',
+    ])
+    ->name('*.php')
+    ->ignoreVCS(true);
 
 return (new Config())
-  ->setRiskyAllowed(true)
-  ->setRules([
-    '@PSR12' => true,
-    'indentation_type' => true,
-    'phpdoc_indent' => true,
-    'curly_braces_position' => [
-      'functions_opening_brace'          => 'same_line',
-      'classes_opening_brace'            => 'same_line',
-      'anonymous_classes_opening_brace'  => 'same_line',
-      'control_structures_opening_brace' => 'same_line',
-      'anonymous_functions_opening_brace' => 'same_line',
-    ],
-  ])
-  ->setFinder($finder);
+    ->setRiskyAllowed(true)
+    ->setIndent("\t")
+    ->setLineEnding("\n")
+    ->setRules([
+        '@PhpCsFixer' => true,
+        '@PHP80Migration' => true,
+        '@PHP81Migration' => true,
+        'declare_strict_types' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'blank_line_before_statement' => [
+            'statements' => ['return', 'throw', 'try'],
+        ],
+        'global_namespace_import' => [
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+        'linebreak_after_opening_tag' => false,
+        'no_unused_imports' => true,
+        'ordered_imports' => [
+            'imports_order' => ['class', 'function', 'const'],
+            'sort_algorithm' => 'alpha',
+        ],
+        'phpdoc_summary' => false,
+        'single_line_empty_body' => false,
+        'single_quote' => true,
+        'yoda_style' => false,
+        'indentation_type' => true,
+    ])
+    ->setFinder($finder)
+    ->setCacheFile(__DIR__ . '/var/.php-cs-fixer.cache');
