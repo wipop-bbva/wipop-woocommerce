d6b88fa71922ab27acce7002d2bad4a6da76e1f2
Víctor de Vierna <victor.vierna@gmail.com>
2025-11-28 19:32:42 +0100

    feat(wp): enable payment options within WooCommerce Blocks pages

---
+++ wipop/core/WooCommerce/Blocks/CardPaymentMethod.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WipopWC\Core\WooCommerce\Blocks;
+
+use WipopWC\Gateways\Card\Gateway;
+
+use function __;
+
+class CardPaymentMethod extends AbstractBlockPaymentMethod
+{
+	protected $name = Gateway::ID;
+
+	public function __construct()
+	{
+		parent::__construct(
+			'woocommerce_' . Gateway::ID . '_settings',
+			__('Card (BBVA)', 'wipop'),
+			__('Paga con Card', 'wipop'),
+			'gateways/card/assets/img/credit-card-svgrepo-com.svg'
+		);
+	}
+}
